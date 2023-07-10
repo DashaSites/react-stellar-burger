f9ef879305d2af573dee438d760d4476643af4bd
Dasha Kholobaeva <dashaholobaeva@gmail.com>
2023-07-11 01:42:56 +0300

    fixed upon first review

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -6,29 +6,24 @@ import BurgerConstructor from "../burger-constructor/burger-constructor.jsx";
 import Modal from "../modal/modal.jsx";
 import IngredientDetails from "../ingredient-details/ingredient-details.jsx";
 import OrderDetails from "../order-details/order-details.jsx";
+import getIngredients from "../../utils/burger-api.js";
 
 const App = () => {
   
-
-  const url = 'https://norma.nomoreparties.space/api/ingredients';
-
   const [ingredients, setIngredients] = React.useState([]);
   const [ingredientToShow, setIngredientToShow] = React.useState({});
 
-  // Достаем данные через запрос к api
+  // Достаю данные через запрос к api: импортирую сюда запрос и ответ из burger-api.js
+  // и обрабатываю эти данные дальше (записываю их в стейт)
   React.useEffect(() => {
-    fetch(`${url}`, {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(res => res.json())
+    getIngredients()
     .then(res => setIngredients(res.data))
     .catch(err => console.log(err));
   }, []);
 
 
-  // Настраиваю состояние и работу модальных окон
+  /// Настраиваю состояние и работу модальных окон ///
+
   // стейт для модального окна IngredientDetails 
   const [
     isIngredientDetailsOpened, 
@@ -47,12 +42,7 @@ const App = () => {
     setIsOrderDetailsOpened(false);
   }
 
-
-  //const handleEscKeydown = (event) => {
-  //  event.key === "Escape" && closeModals();
-  // }
-
-  
+  // Обработка кликов //
   const handleClickIngredient = (ingredient) => {
     setIngredientToShow(ingredient);
     setIsIngredientDetailsOpened(true);
